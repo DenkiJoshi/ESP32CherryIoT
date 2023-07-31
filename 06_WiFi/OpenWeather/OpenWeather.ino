af6157ea2ef4ce10b94a963d4aab4bdabc848960
denkijoshi <faa@paralyze.main.jp>
2023-07-31 13:39:08 +0900

    add

---
+++ 06_WiFi/OpenWeather/OpenWeather.ino
@@ -0,0 +1,54 @@
+//https://home.openweathermap.org/
+//https://openweathermap.org/current
+
+#include <WiFi.h>
+#include <HTTPClient.h>
+ 
+const char* ssid = "AP01-01";
+const char* password =  "1qaz2wsx";
+ 
+const String endpoint = "http://api.openweathermap.org/data/2.5/weather?q=matsue,jp&APPID=";
+//const String key = "YOUR API";
+const String key = "17f83f2e9a9be2915f1204dd14cfac53";
+ 
+void setup() {
+ 
+  Serial.begin(115200);
+ 
+  WiFi.begin(ssid, password);
+ 
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(1000);
+    Serial.println("Connecting to WiFi..");
+  }
+ 
+  Serial.println("Connected to the WiFi network");
+ 
+}
+ 
+void loop() {
+ 
+  if ((WiFi.status() == WL_CONNECTED)) {
+ 
+    HTTPClient http;
+ 
+    http.begin(endpoint + key); //URLを指定
+    int httpCode = http.GET();  //GETリクエストを送信
+ 
+    if (httpCode > 0) { //返答がある場合
+ 
+        String payload = http.getString();  //返答（JSON形式）を取得
+        Serial.println(httpCode);
+        Serial.println(payload);
+      }
+ 
+    else {
+      Serial.println("Error on HTTP request");
+    }
+ 
+    http.end(); //Free the resources
+  }
+ 
+  delay(30000);   //30秒おきに更新
+ 
+}
