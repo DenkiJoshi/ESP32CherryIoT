f565a423c5714f8d95224fe3622619cc59f5bf2d
denkijoshi <faa@paralyze.main.jp>
2024-01-29 15:59:15 +0900

    DHT11 -> DHT20

--- 02_IN_MajorSensors/02-05_Humidity/02-05_Humidity.ino
+++ 02_IN_MajorSensors/02-05_Humidity/02-05_Humidity.ino
@@ -1,39 +1,29 @@
-#include <DHT.h>              // ライブラリのインクルード
+#include <DHT20.h> // ライブラリのインクルード
+
+DHT20 DHT(&Wire1); // センサーの初期化
  
-#define DHT_PIN 33            // DHT11のDATAピンをデジタルピン19に定義
-#define DHT_MODEL DHT11       // 接続するセンサの型番を定義する(DHT11やDHT22など)
- 
- DHT dht(DHT_PIN, DHT_MODEL);  // センサーの初期化
- 
- 
- 
- void setup() {
- 
-   Serial.begin(115200);         // シリアル通信の開始
-   dht.begin();                // センサーの動作開始
-  
+void setup() {
+  Serial.begin(115200); // シリアル通信の開始
+  Wire1.begin(33, 32); // センサーの動作開始
+
+  delay(2000); // センサー動作安定まで待つ
 }
+
+void loop(){
+  delay(2000);                // センサーの読み取りを2秒間隔に
+
+  DHT.read(); 
+  float Humidity = DHT.getHumidity();          // 湿度の読み取り
+  float Temperature = DHT.getTemperature();    // 温度の読み取り(摂氏)
  
- 
- void loop(){
- 
-   delay(2000);                // センサーの読み取りを2秒間隔に
- 
-   float Humidity = dht.readHumidity();          // 湿度の読み取り
-   float Temperature = dht.readTemperature();    // 温度の読み取り(摂氏)
- 
-   if (isnan(Humidity) || isnan(Temperature)) {  // 読み取りのチェック
-     Serial.println("ERROR");
-     return;
-   }
- 
-// シリアルモニタに温度&湿度を表示  
-   Serial.print("温度: ");
-   Serial.print(Temperature);
-   Serial.print("[℃]");
- 
-   Serial.print("  湿度: "); 
-   Serial.print(Humidity);  
-   Serial.println("[%]");
- 
+  // シリアルモニタに温度&湿度を表示  
+  Serial.print("温度: ");
+  Serial.print(Temperature);
+  Serial.print("[℃]");
+
+  Serial.print("  ");
+
+  Serial.print("湿度: "); 
+  Serial.print(Humidity);  
+  Serial.println("[%]");
 }
