ea214b798b6af1f59408db991329c3df0752b7c4
denkijoshi <faa@paralyze.main.jp>
2025-05-27 11:52:22 +0900

    Change library

--- 02_Basicset_IO/11_OLED/11_OLED.ino
+++ 02_Basicset_IO/11_OLED/11_OLED.ino
@@ -1,16 +1,36 @@
-#include <SSD1306.h> //ESP8266 and ESP32 OLED driver for SSD1306 displays
 #include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h> //by Adafruit
 
-SSD1306 display(0x3c, 1, 3); //(I2C, SDA, SCL) 1,3:ConnectorA 5,4:ConnectorB
- 
-void setup() {
-  display.init(); //Initialization
-  display.setFont(ArialMT_Plain_24); //Set Font
-  display.drawString(0, 0, "Hello World"); //1st low
-  display.drawString(0, 21, "Hello World"); //2nd low
-  display.drawString(0, 42, "Hello World"); //3rd low
-  display.display(); //Draw
+#define SCREEN_WIDTH 128
+#define SCREEN_HEIGHT 64
+#define OLED_RESET     -1
+#define SCREEN_ADDRESS 0x3C
+
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
+
+void setup()
+{
+  Wire.begin(5, 4); //(SDA, SCL) 1,3:ConnectorA 5,4:ConnectorB
+
+  if(!display.begin(SSD1306_SWITCHCAPVCC, SCREEN_ADDRESS)) {
+    for(;;); //Infinite loop when screen initialization fails
+  }
+
+  display.clearDisplay();
+
+  display.setTextSize(2);
+  display.setTextColor(SSD1306_WHITE);
+  display.setCursor(0, 5);
+  display.print(F("Hello"));
+  display.setCursor(0, 25);
+  display.print(F("ESP32"));
+  display.setCursor(0, 45);
+  display.print(F("CherryIoT"));
+
+  display.display();
 }
- 
-void loop() {
+
+void loop()
+{
 }
