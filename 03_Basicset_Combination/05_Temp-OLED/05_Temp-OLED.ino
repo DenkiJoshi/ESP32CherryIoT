203ccedb811eb0a021e923bd3115507f18cca4d5
denkijoshi <faa@paralyze.main.jp>
2025-05-27 11:53:44 +0900

    Change OLED's library

--- 03_Basicset_Combination/05_Temp-OLED/05_Temp-OLED.ino
+++ 03_Basicset_Combination/05_Temp-OLED/05_Temp-OLED.ino
@@ -1,10 +1,17 @@
 //C3 only has one I2C channel
-#include <DHT20.h> //DHT20 by RobTillaart
-#include <SSD1306.h> //ESP8266 and ESP32 OLED driver for SSD1306 displays
 #include <Wire.h>
+#include <DHT20.h> //DHT20 by RobTillaart
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h> //by Adafruit
 
 DHT20 DHT;
-SSD1306 display(0x3c, 5, 4); //OLED(I2C, SDA, SCL) 1,3:ConnectorA 5,4:ConnectorB
+
+#define SCREEN_WIDTH 128
+#define SCREEN_HEIGHT 64
+#define OLED_RESET     -1
+#define SCREEN_ADDRESS 0x3C
+
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
 
 void setup() {
   Serial.begin(115200);
@@ -24,11 +31,19 @@ void loop() {
   Wire.end();
   
   //Start OLED
-  Wire.begin(5, 4); //OLED(SDA, SCL)1,3:ConnectorA 5,4:ConnectorB 
-  display.init(); //Initialization
-  display.setFont(ArialMT_Plain_24); //Set Font
-  display.drawString(0, 0, String(Temperature) + "°C"); //1st low
-  display.drawString(0, 21, String(Humidity) + "%"); //2nd low
+  Wire.begin(5, 4); //OLED(SDA, SCL)1,3:ConnectorA 5,4:ConnectorB
+
+  if(!display.begin(SSD1306_SWITCHCAPVCC, SCREEN_ADDRESS)) {
+    for(;;); //Infinite loop when screen initialization fails
+  }
+
+  display.clearDisplay();
+  display.setTextSize(2);
+  display.setTextColor(SSD1306_WHITE);
+  display.setCursor(0, 5);
+  display.print(String(Temperature) + "'C"); //1st low
+  display.setCursor(0, 25);
+  display.print(String(Humidity) + "%"); //2nd low
   display.display(); //Draw
   delay(1000);
   Wire.end();
