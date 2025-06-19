d516829de5e8a0da9663602afd601658c9b2290f
DenkiJoshi <faa@paralyze.main.jp>
2025-06-19 11:34:05 +0900

    Change to LINE message api

--- 04_Basicset_Wifi/07_API_LINE/07_API_LINE.ino
+++ 04_Basicset_Wifi/07_API_LINE/07_API_LINE.ino
@@ -1,86 +1,77 @@
-//https://www.ekit-tech.com/?p=3434
+//https://github.com/bokunimowakaru/line/blob/master/esp32/ex01_basic/ex01_basic.ino
 
 #include <WiFi.h>
-#include <WiFiClientSecure.h>
+#include <HTTPClient.h>
+ 
+#define line_ch_id "xxxxx"                         // Channel ID
+#define line_ch_pw "xxxxx"   // Channel secret
 
-// WiFi Setting
-const char* ssid     = "xxxxx";
-const char* password = "xxxxx";
+#define SSID "xxxxx"
+#define PASS "xxxxx"
 
-// LINE Notify Setting
-const char* host = "notify-api.line.me";
-const char* token = "xxxxxxxxxxxxxxxxxxxx"; //Your API key
-const char* message = "Hello, I'm ESP32";
+String get_token(){
+    HTTPClient http;  // HTTP request instance
+    if(strcmp(line_ch_id,"0000000000") == 0){  // When line_ch_id is not entered
+        Serial.println("ERROR: Pls set line_ch_id and pwd");
+        return "";
+    }
+    String url = "https://api.line.me/oauth2/v3/token";
+    String body = "grant_type=client_credentials&";
+    body += "client_id=" + String(line_ch_id) + "&";
+    body += "client_secret=" + String(line_ch_pw);
+    http.begin(url);  // Set the HTTP request destination
+    http.addHeader("Content-Type","application/x-www-form-urlencoded");
+    int httpCode = http.POST(body);  // Send HTTP
+    String token="";
+    if(httpCode == 200){
+        String S = http.getString();  // Store the HTTP response body (text such as JSON)
+        int i = S.indexOf("\"access_token\"");
+        if((i>0) && (S.substring(i+15, i+16).equals("\""))){  // Find the "access_token" in the JSON string
+            token = S.substring(i+16, i+16+174);
+            Serial.println(token);
+        }
+    }else{
+        Serial.println("HTTP ERROR: "+String(httpCode));
+    }
+    http.end();  // End HTTP communication
+    return token;
+}
 
-const int swPin = 3; //3:ConnectorA 4:ConnectorB
+int message_to_line(String message){
+    String token = get_token();  // Get a token
+    int token_len = token.length();  // The length of the token obtained
+    if(token_len != 174){  // Warning when token is too long
+        Serial.println("ERROR: Token Length Error; " + String(token_len));
+        return 0;
+    }
+    HTTPClient http;  // HTTP request instance
+    String url = "https://api.line.me/v2/bot/message/broadcast";
+    Serial.println(url);
+    http.begin(url);  // Set the HTTP request destination
+    http.addHeader("Content-Type","application/json");
+    http.addHeader("Authorization","Bearer " + token);
+    String json = "{\"messages\":[{\"type\":\"text\",\"text\":\"";
+    json += message + "\"}]}";  // Assign message to json for sending
+    Serial.println(json);
+    int httpCode = http.POST(json);  // Sending messages over HTTP
+    http.end();  // End HTTP communication
+    return httpCode;  // HTTP result
+}
 
 void setup(){
-  Serial.begin(115200);
-  pinMode(swPin,INPUT);
+    Serial.begin(115200);
+    Serial.println("LINE Messaging API");
 
-  connectWiFi();
+    WiFi.mode(WIFI_STA);  // WiFi to STA mode
+    WiFi.begin(SSID,PASS);  // WiFi access point connection
+    while(WiFi.status() != WL_CONNECTED){
+        Serial.print('.');
+        delay(500);
+    }
+    Serial.println(WiFi.localIP());
 }
 
 void loop(){
-  if (digitalRead(swPin)) {
-    send_line();
-  }
-}
-
-void connectWiFi(){
-  Serial.println();
-  Serial.print("Connecting to ");
-  Serial.println(ssid);
-
-  //Start WiFi connection
-  WiFi.begin(ssid, password);
-
-  //Check WiFi connection status
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
-    Serial.print(".");
-  }
-
-  Serial.println("");
-  Serial.println("WiFi connected.");
-  Serial.println("IP address: ");
-  Serial.println(WiFi.localIP());
-}
-
-void send_line() {
-  //Access to HTTPS (SSL communication)
-  WiFiClientSecure client;
-  //Required when connecting without verifying the server certificate
-  client.setInsecure();
-
-  Serial.println("Try");
-
-  //SSL connection to Line's API server (port 443: https)
-  if (!client.connect(host, 443)) {
-    Serial.println("Connection failed");
-    return;
-  }
-  Serial.println("Connected");
-
-  // Send request
-  String query = String("message=") + String(message);
-  String request = String("") +
-    "POST /api/notify HTTP/1.1\r\n" +
-    "Host: " + host + "\r\n" +
-    "Authorization: Bearer " + token + "\r\n" +
-    "Content-Length: " + String(query.length()) +  "\r\n" + 
-    "Content-Type: application/x-www-form-urlencoded\r\n\r\n" +
-    query + "\r\n";
-  client.print(request);
- 
-  // Wait until reception is complete
-  while (client.connected()) {
-    String line = client.readStringUntil('\n');
-    if (line == "\r") {
-      break;
-    }
-  }
-  
-  String line = client.readStringUntil('\n');
-  Serial.println(line);
+    message_to_line("[esp32] Hello!");  // Free Message:200/Month
+    delay(10*60*1000);  // 10 minutes
 }
