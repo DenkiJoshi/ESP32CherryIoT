e8e89ee9e1a8386c14b80a975006ce27fedd9b3a
DenkiJoshi <faa@paralyze.main.jp>
2026-05-07 13:57:53 +0900

    add skip verification code

--- 04_Basicset_Wifi/12_Googlespreadsheet_LightSensor/12_Googlespreadsheet_LightSensor.ino
+++ 04_Basicset_Wifi/12_Googlespreadsheet_LightSensor/12_Googlespreadsheet_LightSensor.ino
@@ -3,9 +3,9 @@
 #include <WiFi.h>
 #include <WiFiClientSecure.h>
 
-// 接続先のSSIDとパスワード
-const char* ssid = "xxxxxx"; //無線ルーターのssidを入力
-const char* password = "xxxxxx"; //無線ルーターのパスワードを入力
+// WiFi Setting
+const char* ssid = "xxxxxx"; 
+const char* password = "xxxxxx";
 
 const int litsnsrPin = 3; //3:ConnectorA 4:ConnectorB
 
@@ -14,46 +14,39 @@ void setup() {
   pinMode(litsnsrPin, INPUT);
 }
 
-
 void loop() {
   sendData();
   delay(10000);
 }
 
-
-//WiFiに接続
 void connectWiFi(){
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
 
-  Serial.print("ssid:");
-  Serial.print(ssid);
-  Serial.println(" に接続します。");
-
+  //Start WiFi connection
   WiFi.begin(ssid, password);
 
-  Serial.print("WiFiに接続中");
-  while(WiFi.status() != WL_CONNECTED) {
-    Serial.print(".");
+  //Check WiFi connection status
+  while (WiFi.status() != WL_CONNECTED) {
     delay(500);
+    Serial.print(".");
   }
 
-  Serial.println("接続しました。");
-
-  //IPアドレスの表示
-  Serial.print("IPアドレス:");
+  Serial.println("");
+  Serial.println("WiFi connected.");
+  Serial.println("IP address: ");
   Serial.println(WiFi.localIP());
 }
 
- 
-
-//測定データの送信
 void sendData(){
 
   WiFiClientSecure sslclient;
 
   const char* server = "script.google.com";
-  String url = "https://script.google.com/macros/s/xxxxxxxxxxxxxx/exec";  //googlescript web appのurlを入力
+  String url = "https://script.google.com/macros/s/xxxxxxxxxxxxxx/exec";  //googlescript web app url
 
-  //測定値を準備
+  //Prepare measurements
   float litsnsr_ad = analogRead(litsnsrPin); // Read analog data
   float litsnsr_v = litsnsr_ad * 3.3 / 4096; // Calculation of voltage value
   float lux = 10000 * litsnsr_v / (3.3 - litsnsr_v) / 1000; // Calculation of lux value
@@ -63,38 +56,37 @@ void sendData(){
   
   float sensor_data1= lux;
 
-  //wifiに接続
+  //Connect WiFi
   connectWiFi();
 
-  //測定値の表示
+  //Display of measured values
   Serial.println(sensor_data1);
   
-
-  //urlの末尾に測定値を加筆
+  //Add measurement value to the end of the URL
   url += "?";
   url += "&1_cell=";
   url += sensor_data1;
  
+  //Access server
+  Serial.println("Access server...");
+  sslclient.setInsecure(); //skip verification
 
-  // サーバーにアクセス
-  Serial.println("サーバーに接続中...");
-
-  //データの送信
+  //Send data
   if (!sslclient.connect(server, 443)) {
-    Serial.println("接続に失敗しました");
-    Serial.println("");//改行
+    Serial.println("Connection failed!");
+    Serial.println("");
     return;
   }
 
-  Serial.println("サーバーに接続しました");
+  Serial.println("Connected to server");
 
   sslclient.println("GET " + url);
-  delay(1000); //私の環境ではここに待ち時間を入れないとデータが送れないことがある
+  delay(1000); //Waiting for stabilization
   sslclient.stop();
 
-  Serial.println("データ送信完了");
-  Serial.println("");//改行
+  Serial.println("Data transmission completed");
+  Serial.println("");
 
-  //WiFiを切断
+  //Disconnect WiFi
   WiFi.mode(WIFI_OFF);
 }
