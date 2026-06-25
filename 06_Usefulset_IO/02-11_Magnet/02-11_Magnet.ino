09ded2e4232c57b3577c01ef5f4d476b521f59b8
DenkiJoshi <faa@paralyze.main.jp>
2026-06-25 11:16:49 +0900

    chenge pin number

--- 06_Usefulset_IO/02-11_Magnet/02-11_Magnet.ino
+++ 06_Usefulset_IO/02-11_Magnet/02-11_Magnet.ino
@@ -1,18 +1,16 @@
-#define hallpin 32 //INA:33 INB:32
+const int hallPin = 3; //3:ConnectorA 4:ConnectorB
 
 void setup() {
   Serial.begin(115200);
-  pinMode(hallpin,INPUT);
-  
+  pinMode(hallPin, INPUT);
 }
 
 void loop() {
-  int base = 1650; // ゼロガウス(0G) = 2.5V(2500mV)
-   // アナログ入力の値を電圧に変換(mV)
-  float voltage =  (analogRead(hallpin) / 4096.0) * 3.3 * 1000;
-  float gauss = abs((voltage - base) / 3.3); // 1ガウス(1G) = 5mV
+  int base = 3300; // Zero Gauss(0G) = 3.3V(3300mV)
+   // Convert the analog input value to voltage.(mV)
+  float voltage =  (analogRead(hallPin) / 4096.0) * 3.3 * 1000;
+  float gauss = abs((voltage - base) / 3.3); // 1Gauss(1G) = 3.3mV
   Serial.print(gauss);
   Serial.println("gauss");
-  delay(500);
-  //磁石のプラスチック側（N極）をHallセンサーの「Hall」シルク側へ近づけるとgauss値が高くなる。 
+  delay(500); 
 }
